08b8e6c46e8eb8eeff07c22971716adbe2307e50
koko <koko@drones.gay>
2023-09-25 14:41:09 -0400

    Revert "Throw if host is unreachable during instantiation"
    This reverts commit 647edde579f3410edb7ef002ad240591dc7a58f5.

--- Starnet/LighthouseClient.cs
+++ Starnet/LighthouseClient.cs
@@ -33,17 +33,8 @@ public class LighthouseClient : IDisposable
         // Set the user agent of the HTTP Client.
         this.httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("LighthouseClient", "1.0"));
 
-        // Validate that the host is reachable.
-        if (!this.IsHostReachable())
-        {
-            throw new ApiConnectionException("The server is unreachable. Did you specify the correct host?");
-        }
-
         // Set the authentication key of the HTTP Client, if provided.
-        if (authenticationToken == null)
-        {
-            return;
-        }
+        if (authenticationToken == null) return;
 
         this.httpClient.DefaultRequestHeaders.Authorization = authenticationToken.StartsWith("$")
             ? new AuthenticationHeaderValue(authenticationToken)
@@ -60,12 +51,6 @@ public class LighthouseClient : IDisposable
     }
 
     #region Status requests
-
-    /// <summary>
-    ///     Checks if the host is reachable.
-    /// </summary>
-    /// <returns>Boolean</returns>
-    internal bool IsHostReachable() => this.GetStatusAsync().Result == 200;
 
     /// <summary>
     ///     Gets the status of the server.
